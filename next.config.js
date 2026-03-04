6609e281a675aecc06f69ce5cead23253473dfb1
Mark Olayabr <markolaybar@gmail.com>
2026-03-04 13:51:07 +0800

    Fix next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
