f09c8f4041032c221909f81a8ae895dbbb8d19de
Mark Olayabr <markolaybar@gmail.com>
2026-03-07 15:34:50 +0800

    fix: overflow-y-auto on main so sticky topbar doesn't overlap content

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -23,7 +23,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-background">
       <Sidebar workspace={workspace} />
-      <main className="flex-1 flex flex-col min-w-0 pb-20 lg:pb-0">
+      <main className="flex-1 flex flex-col min-w-0 pb-20 lg:pb-0 overflow-y-auto h-screen">
         {children}
       </main>
       <MobileNav />
